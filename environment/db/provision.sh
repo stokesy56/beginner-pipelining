f920b1e89579ab44a5792f8ead7e852e880ccb5e
Rory Stokes <RStokes@spartaglobal.com>
2019-10-15 17:07:49 +0100

    asd

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -6,7 +6,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# sudo apt-get install mongodb-org=3.2.18 -y
+# sudo apt-get install mongodb-org=3.2.20 -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20 --allow-unauthenticated
 
 sudo rm /etc/mongod.conf
